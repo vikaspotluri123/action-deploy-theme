adce368fa3c1b8b014b0910e153449e96cd08289
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-01-07 17:04:30 -0600

    Add support for passing in zip file

--- index.js
+++ index.js
@@ -14,13 +14,21 @@ const GhostAdminApi = require('@tryghost/admin-api');
 
         const basePath = process.env.GITHUB_WORKSPACE;
         const pkgPath = path.join(process.env.GITHUB_WORKSPACE, 'package.json');
-        const themeName = core.getInput('theme-name') || require(pkgPath).name;
-        const themeZip = `${themeName}.zip`;
-        const zipPath = path.join(basePath, themeZip);
-        const exclude = core.getInput('exclude') || '';
+        
+        let zipPath = core.getInput('file');
+        
+        // Zip file was not provided - zip everything up!
+        if (!zipPath) {
+            const themeName = core.getInput('theme-name') || require(pkgPath).name;
+            const themeZip = `${themeName}.zip`;
+            const exclude = core.getInput('exclude') || '';
+            zipPath = themeZip;
 
-        // Create a zip
-        await exec.exec(`zip -r ${themeZip} . -x *.git* *.zip yarn* npm* *routes.yaml *redirects.yaml *redirects.json ${exclude}`, [], {cwd: basePath});
+            // Create a zip
+            await exec.exec(`zip -r ${themeZip} . -x *.git* *.zip yarn* npm* *routes.yaml *redirects.yaml *redirects.json ${exclude}`, [], {cwd: basePath});
+        }
+
+        zipPath = path.join(basePath, themeZip);
 
         // Deploy it to the configured site
         await api.themes.upload({file: zipPath});
